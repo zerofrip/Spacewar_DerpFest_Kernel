3d256b63703796c4df0cdf48798deae886ee01c5
zerofrip <zerofripside@gmail.com>
2025-02-26 14:57:04 +0900

    Update KernelSU-Next

--- setup.sh
+++ setup.sh
@@ -39,11 +39,11 @@ perform_cleanup() {
 # Sets up or update KernelSU-Next environment
 setup_kernelsu() {
     echo "[+] Setting up KernelSU-Next..."
-    test -d "$GKI_ROOT/KernelSU-Next" || git clone https://github.com/rifsxd/KernelSU-Next && echo "[+] Repository cloned."
+    test -d "$GKI_ROOT/KernelSU-Next" || git clone https://github.com/KernelSU-Next/KernelSU-Next && echo "[+] Repository cloned."
     cd "$GKI_ROOT/KernelSU-Next"
     git stash && echo "[-] Stashed current changes."
     if [ "$(git status | grep -Po 'v\d+(\.\d+)*' | head -n1)" ]; then
-        git checkout next-susfs && echo "[-] Switched to next-susfs branch."
+        git checkout next && echo "[-] Switched to next branch."
     fi
     git pull && echo "[+] Repository updated."
     if [ -z "${1-}" ]; then
